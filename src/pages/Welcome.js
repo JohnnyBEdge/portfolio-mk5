7b6c21481be9419ac268826c370d225d1079ad8f
John Martin <jrmartin182@gmail.com>
2020-05-17 18:06:18 -0600

    added fade in to welcome text

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Nav from './Nav';
 import '../comp-style/welcome.css';
-import {bounce} from 'react-animations';
+import Fade from 'react-reveal/Fade';
 
 
 const Welcome = () =>{
     return (
             <div id="welcome" className="section">
-            <Nav />
+                <Fade duration={3000}>
+                <Nav />
                 <div id="sum_container">
-                    <h1 className = "animate__animated animate__bounce">Full-Stack Javascript Developer</h1>
+                    <h1>Full-Stack Javascript Developer</h1>
                     <p id="summary">My name is John and I am a web developer based out of Salt Lake City, Utah. </p>
                 </div>
+                </Fade>
             </div>
     );
 
